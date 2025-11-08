e6e1e5f72a79b7e96f07150d3c56e0a4511b40dc
itspe <prithvi.kumar@itspe.co.in>
2025-11-08 13:34:59 +0530

    updated databse.js

--- autodialer-app/config/database.js
+++ autodialer-app/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/autodialer',
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     logging: false,
